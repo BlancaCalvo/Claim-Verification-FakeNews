3930ddaca0a620a2e9ccfb4fb1915e587c7f2ce8
Blanca Calvo Figueras <bcalvo@CPU00256U.vicomtech.es>
2020-11-30 09:42:50 +0100

    Change in evaluation metrics to match MultiFC paper

--- experiment-2/GEAR-MultiFC/my_scripts/evaluation.py
+++ experiment-2/GEAR-MultiFC/my_scripts/evaluation.py
@@ -1,7 +1,8 @@
 import numpy as np
 #from fever.scorer import fever_score
 import json
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+import re
 
 ENCODING = 'utf-8'
 
@@ -10,12 +11,30 @@ def get_predicted_label(items):
     #print(labels[np.argmax(np.array(items))])
     return labels[np.argmax(np.array(items))]
 
+def indices(l, val):
+    retval = []
+    last = 0
+    while val in l[last:]:
+        i = l[last:].index(val)
+        retval.append(last + i)
+        last += i + 1
+    return retval
+
+def class_accuracy(l, y_pred, y_true, classes):
+    index = indices(l,classes)
+    y_pred, y_true = np.array(y_pred)[index], np.array(y_true)[index]
+    macro = f1_score(y_pred, y_true, average='macro')
+    micro = f1_score(y_pred, y_true, average='micro')
+    return micro, macro, len(y_pred)
+
 def dev_scorer(truth_file, output_file):
     fin = open(truth_file, 'rb')
 
     truth_list = []
+    domain = []
     for claim_num, line in enumerate(fin):
         claim_id, label, claim = line.decode(ENCODING).strip('\r\n').split('\t')[:3]
+        domain.append(re.sub('-[0-9]{5}$', '', claim_id))
         truth_list.append(label)
     fin.close()
 
@@ -30,11 +49,19 @@ def dev_scorer(truth_file, output_file):
         answers.append(get_predicted_label(result))
     fin.close()
 
-    #print(answers)
+    macro_list = micro_list = 0
+    print('Micro_F1', 'Macro_F1', 'Num_Instances')
+    for d in set(domain):
+        micro, macro, instances = class_accuracy(domain, truth_list, answers, d)
+        print(d,micro,macro, instances)
+        macro_list+=macro
+        micro_list+=micro
+    print('Average', micro_list / len(set(domain)),macro_list/len(set(domain)))
 
-    print(accuracy_score(truth_list, answers))
-    print(confusion_matrix(truth_list, answers))
-
+    #print(accuracy_score(truth_list, answers))
+    #print(confusion_matrix(truth_list, answers))
+    #print(f1_score(truth_list, answers, average='macro'))
+    #print(f1_score(truth_list, answers, average='micro'))
 
 
 
