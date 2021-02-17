81077b4f7f558ef008fa4f6d27b1f5bc8f3cc8e0
Blanca Calvo Figueras <bcalvo@CPU00256U.vicomtech.es>
2021-02-17 09:42:56 +0100

    raw srl

--- experiment-4/SRL_extraction.py
+++ experiment-4/SRL_extraction.py
@@ -2,6 +2,9 @@ import itertools
 import logging
 import re
 from extractor import InputExample
+import argparse
+from allennlp.predictors import Predictor
+import json
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
@@ -127,3 +130,46 @@ def read_examples_SRL_1claim(input_file, predictor):
                                           'label': label, 'index': index, 'is_claim': False})
                         unique_id += 1
     return examples, json_list
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", default=None, type=str, required=True)
+    parser.add_argument("--output_file", default='data/graph_features/srl_features.json', type=str, required=True)
+    parser.add_argument("--cuda", default=-1, type=int, required=False)
+    args = parser.parse_args()
+    json_list = []
+    unique_id = line_n = 0
+    predictor = Predictor.from_path(
+        "https://storage.googleapis.com/allennlp-public-models/bert-base-srl-2020.11.19.tar.gz", cuda_device=args.cuda)
+
+    num_lines = sum(1 for line in open(args.input_file))
+    with open(args.input_file, "r", encoding='utf-8') as reader:
+        while True:
+            line = reader.readline()
+            logger.info("Parsing input with SRL: {}/{}".format(line_n, num_lines))
+            line_n += 1
+            if not line:
+                break
+            line = line.strip().split('\t')
+            index = line[0]
+            label = line[1]
+            claim = line[2]
+            evidences = line[3:]
+            claim_prediction = predictor.predict_json({'sentence': claim})['verbs']
+
+            for evidence in evidences:
+                evidence = re.sub(r'\.[a-zA-Z \-é0-9\(\)]*$', '',
+                                  evidence)  # instead of this line I should change the build_gear_input_set.py script
+                try:
+                    prediction = predictor.predict_json({'sentence': evidence})
+                except RuntimeError:
+                    print('Length issue with this evidence: ', evidence)
+                    continue
+
+                json_list.append(
+                    {'unique_id': unique_id, 'claim': claim, 'claim_srl': claim_prediction, 'evidence': evidence, 'evidence_srl': prediction['verbs'],
+                     'label': label, 'index': index})
+                unique_id += 1
+
+    with open(args.output_file, 'w') as fout:
+        json.dump(json_list, fout)
